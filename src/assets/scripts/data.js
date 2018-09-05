845165cd42cd434a7eda569461bc56debc9f11ad
Евгений Щербаков <Scherbacov1993evgeniy@yandex.ru>
2018-09-05 20:05:44 +0300

    without API

--- src/assets/scripts/data.js
+++ src/assets/scripts/data.js
@@ -3,21 +3,12 @@ export function loadReviews() {
   return JSON.parse(reviewsMap);
 }
 
-export function addPlacemark(ymaps, coords /*, balloonLayout, reviews*/) {
+export function addPlacemark(ymaps, coords) {
   return (window.myPlacemark = new ymaps.Placemark(
     coords,
-    {
-      /*reviews,
-      lat: element.lat,
-      long: element.long,
-      name: element.name,
-      place: element.place,
-      date: element.date,
-      comment: element.comment*/
-    },
+    {},
     {
       balloonShadow: false,
-      //balloonLayout: balloonLayout,
       balloonPanelMaxMapArea: 0,
       hideIconOnBalloonOpen: false,
       preset: "islands#violetDotIcon"
--- src/assets/scripts/map.js
+++ src/assets/scripts/map.js
@@ -125,19 +125,6 @@ export default function initMap(ymaps, containerId) {
     }
   });
 
-  //открываем балун по клику на карте через API
-  /*myMap.events.add("click", function(e) {
-    if (!myMap.balloon.isOpen()) {
-      var coords = e.get("coords");
-      myMap.balloon.open(coords, {
-        balloonLayout: BalloonContentLayout
-        // [coords[0].toPrecision(6), coords[1].toPrecision(6)].join(", ")
-      });
-    } else {
-      myMap.balloon.close();
-    }
-  });*/
-
   const customItemContentLayout = ymaps.templateLayoutFactory.createClass(
     `<div class="ballon__header">
         <div class="title">{{ properties.place }}</div>
@@ -177,140 +164,10 @@ export default function initMap(ymaps, containerId) {
   });
   myMap.geoObjects.add(clusterer);
 
-  //кастомный шаблон балуна метки на API
-  /*const BalloonContentLayout = ymaps.templateLayoutFactory.createClass(
-    `<div class="balloon">
-      <div class="balloon__header">
-        <div class="balloon__title"></div>
-        <button class="balloon__close"></button>
-      </div>
-      <div class="balloon__content">
-        <div class="balloon__reviews reviews">
-          <ul class="reviews__list">
-          {% for review in properties.reviews %}
-            <li class="reviews__item">
-              <div class="reviews__author">{{ review.name }}</div>
-              <div class="reviews__place">{{ review.place }}</div>
-              <div class="reviews__date">{{ review.date }}</div>
-              <div class="reviews__text">{{ review.comment }}</div>
-            </li>
-          {% endfor %}
-          </ul>
-        </div>
-        <div class="balloon__form review-form">
-          <form class="review-form__area" id="new-review">
-            <div class="review-form__title">Ваш отзыв</div>
-            <div class="review-form__fields">
-              <div class="review-form__field">
-                <label class="review-form__field-label">Ваше имя</label>
-                <input name="name" class="review-form__field-input" placeholder="Ваше имя"/>
-              </div>
-              <div class="review-form__field">
-                <label class="review-form__field-label">Укажите место</label>
-                <input name="place" class="review-form__field-input" placeholder="Укажите место"/>
-              </div>
-              <div class="review-form__field">
-                <label class="review-form__field-label">Поделитесь впечатлениями</label>
-                <textarea name="comment" class="review-form__field-textarea" placeholder="Поделитесь впечатлениями" rows="5"></textarea>
-              </div>
-            </div>
-            <button class="review-form__button">Добавить</button>
-          </form>
-        </div>
-      </div>
-    </div>`,
-    {
-      build: function() {
-        BalloonContentLayout.superclass.build.call(this);
-
-        this.elemBalloon = this.getElement();
-        let closeBtn = this.elemBalloon.querySelector(".balloon__close");
-        let addBtn = this.elemBalloon.querySelector(".review-form__button");
-        this.coords = [
-          this.getData().properties._data.lat,
-          this.getData().properties._data.long
-        ];
-
-        if (this.coords) {
-          const balloonTitle = this.elemBalloon.querySelector(
-            ".balloon__title"
-          );
-          getAddress(this.coords).then(address => {
-            balloonTitle.innerText = address;
-          });
-        }
-
-        closeBtn.addEventListener("click", this.onCloseClick.bind(this));
-        addBtn.addEventListener("click", this.addReview.bind(this));
-      },
-      clear: function() {
-        let closeBtn = this.elemBalloon.querySelector(".balloon__close");
-        let addBtn = this.elemBalloon.querySelector(".review-form__button");
-
-        closeBtn.removeEventListener("click", this.onCloseClick);
-        addBtn.removeEventListener("click", this.addReview);
-
-        BalloonContentLayout.superclass.clear.call(this);
-      },
-      onCloseClick: function(e) {
-        e.preventDefault();
-        this.events.fire("userclose");
-      },
-      addReview: function(e) {
-        e.preventDefault();
-
-        let formFields = document.forms["new-review"].elements;
-        let elem = {
-          name: formFields.name.value,
-          place: formFields.place.value,
-          comment: formFields.comment.value,
-          date: new Date()
-            .toJSON()
-            .slice(0, 10)
-            .split("-")
-            .reverse()
-            .join(".")
-        };
-
-        if (elem.name && elem.place && elem.comment) {
-          let reviewsList = this.elemBalloon.querySelector(".reviews__list");
-          let item = document.createDocumentFragment();
-          item.innerHTML = `<li class="reviews__item">
-              <div class="reviews__author">{{ elem.name }}</div>
-              <div class="reviews__place">{{ elem.place }}</div>
-              <div class="reviews__date">{{ elem.date }}</div>
-              <div class="reviews__text">{{ elem.comment }}</div>
-            </li>`;
-          reviewsList.appendChild(item);
-
-          [elem.lat, elem.long] = this.coords;
-          const myPlacemark = addPlacemark(ymaps, elem, BalloonContentLayout);
-          clusterer.add(myPlacemark);
-
-          if (!reviewsMap[this.coords.join("-")]) {
-            reviewsMap[this.coords.join("-")] = [];
-          }
-
-          reviewsMap[this.coords.join("-")].push(elem);
-
-          formFields.name.value = "";
-          formFields.place.value = "";
-          formFields.comment.value = "";
-        }
-      }
-    }
-  );*/
-
   //добавление сохранённых меток на карту
   for (let coords in reviewsMap) {
-    reviewsMap[coords].map(elem => {
-      //[elem.lat, elem.long] = coords.split("-");
-      const myPlacemark = addPlacemark(
-        ymaps,
-        coords.split("-")
-        //BalloonContentLayout,
-        //reviewsMap[coords]
-      );
+    reviewsMap[coords].map(() => {
+      const myPlacemark = addPlacemark(ymaps, coords.split("-"));
       clusterer.add(myPlacemark);
     });
   }
